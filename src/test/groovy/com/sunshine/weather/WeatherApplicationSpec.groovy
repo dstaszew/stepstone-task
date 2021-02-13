685ae464a892162cd4442bbcdb9add6201247b79
Didi <staszewska.dt@gmail.com>
2021-02-13 20:13:32 +0100

    Add spock to project

---
+++ src/test/groovy/com/sunshine/weather/WeatherApplicationSpec.groovy
@@ -0,0 +1,11 @@
+package com.sunshine.weather
+
+import spock.lang.Specification
+
+class WeatherApplicationSpec extends Specification {
+
+    def "Main"() {
+        expect:
+        1==1
+    }
+}
